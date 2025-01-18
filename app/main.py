b22544deb05ec150ed4f59c61495d74266bfcc20
nevilpurpp <nevilpurpp12@gmail.com>
2025-01-18 18:01:24 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,7 +7,12 @@ def main():
      sys.stdout.write("$ ")
 
      command = input()
+     if command == "exit 0":
+     #if command.split(" ")[0] == "exit" and command.split(" ")[1]== "0":
+          sys.exit(0)
      print(f"{command}: command not found")
+
+
 
 
 
