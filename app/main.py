6f0d4c85d8692f897614d6ba129cc71bd99cd526
nevilpurpp <nevilpurpp12@gmail.com>
2025-01-19 15:32:29 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,15 +2,18 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
+
     while True:
      sys.stdout.write("$ ")
 
      command = input()
      if command == "exit 0":
-     #if command.split(" ")[0] == "exit" and command.split(" ")[1]== "0":
           sys.exit(0)
-     print(f"{command}: command not found")
+     command.startswith("echo")
+     output = command.lstrip('echo')
+     print(f"{output}")
+
+     #print(f"{command}: command not found")
 
 
 
